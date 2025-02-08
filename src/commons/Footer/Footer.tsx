ab2be6d4c2b49821b042090f51d927070bb93112
jayanta-hub <mahanta.rupesh704@gmail.com>
2025-02-08 15:53:54 +0530

    Footer completed

--- src/commons/Footer/Footer.tsx
+++ src/commons/Footer/Footer.tsx
@@ -4,7 +4,12 @@ type Props = {}
 
 const Footer = (props: Props) => {
     return (
-        <div>Footer</div>
+        <footer className='h-[18rem] bg-ring text-background  flex flex-col gap-3 items-center justify-center'>
+
+
+            <p className='capitalize text-center tracking-wider'>business enquiries?</p>
+            <div className='flex flex-col md:flex-row md:items-end  gap-2 md:gap-3'><p className='text-4xl md:text-5xl font-bold uppercase'>I m ready!</p> <p className='underline  text-xl md:text-2xl flex justify-center md:item-end tracking-wider capitalize text-orange-400'>Let's Chat.</p> </div>
+        </footer>
     )
 }
 
